bb62b51b22af01c917e707a979785dd17248c288
Michael Gottlieb <30715318+mjgottlieb@users.noreply.github.com>
2017-11-26 10:58:48 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions utilitze 'caching' to solve for the inverse of a matrix.
+## This approach helps speed up processing time by splitting up the required CPU work 
+## into separate, isolated efforts.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix() takes a matrix 'x' as an argument and establishes a series 
+## of equations that will be used with a later function to calculate the matrix's 
+## inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        B <- NULL
+        set <- function(y){
+                x <<- y
+                B <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) B <<- inverse
+        getInverse <- function() B
+        list(set = set, get = get, setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
+n <- matrix(floor(runif(16,1,10)),4,4)
+q <- makeCacheMatrix(n)
 
-## Write a short comment describing this function
+## cacheSolve() takes the same matrix 'x' and computer its inverse using the cached functions
+## established in the makeCacheMatrix() function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        B <- x$getInverse()         
+        if(!is.null(B)){            
+                message("getting cached data")
+                return(B)
+        }
+        data <- x$get()
+        B <- solve(data, ...)
+        x$setInverse(B)
+        B
 }
+
+z <- cacheSolve(q)
+
+round(n%*%z)
